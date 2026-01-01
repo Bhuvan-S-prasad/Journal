7baecb105d82381efc2003eed77d7dc6afe9bbfc
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2026-01-01 11:48:30 +0530

    feat: initialized uploadthing for image upload functionality, - modified create-entry page - add shadcn form UI components

--- app/(rest)/create-entry/page.tsx
+++ app/(rest)/create-entry/page.tsx
@@ -1,9 +1,21 @@
 function Page() {
     return (
-        <main className="flex pt-10 justify-center h-screen px-10 md:px-30">
+        <main className="pt-10 h-screen px-10 md:px-15">
             <div className="flex flex-col items-center gap-2">
                 <h1 className="text-2xl font-bold text-foreground">Create Entry</h1>
+
+                <p className="text-muted-foreground text-sm">{new Date().toLocaleString()}</p>
+
+                <p>How are you feeling today?</p>
+
+
             </div>
+            <div className="flex-1">
+
+
+            </div>
+
+
         </main>
     )
 }
---
+++ lib/utils.ts
@@ -0,0 +1,33 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function isBase64Image(imageData: string) {
+  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
+  return base64Regex.test(imageData);
+}
+
+export function formatTimeAgo(date: Date): string {
+  const now = new Date();
+  const diffInSeconds = Math.floor((now.getTime() - new Date(date).getTime()) / 1000);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds}s`;
+  }
+
+  const diffInMinutes = Math.floor(diffInSeconds / 60);
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes}m`;
+  }
+
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours}h`;
+  }
+
+  const diffInDays = Math.floor(diffInHours / 24);
+  return `${diffInDays}d`;
+}
